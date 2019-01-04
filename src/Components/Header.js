35230c3ddf5f2d5a281eac60bc3c0a99ed4aa752
77ccreed <kiur.jullis@gmail.com>
2019-01-04 19:23:25 +0200

    ..

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,11 +8,11 @@ class Header extends Component {
     return (
     
       <header className="App-header">
-        <h1 className="Header-text display-4"><span className="Header-text2" >Valga</span> Rockiklubi</h1>
-        <h3 className="Header-text3 m-2 m-md-5">Aastast 2002</h3>
+        <h1 className="Header-text display-4 m-2 ml-md-5 text-left"><span className="Header-text2" >Valga</span> Rockiklubi</h1>
+        <h3 className="Header-text3 m-2 ml-md-5 text-left">Alternatiivklubi, kus propageeritakse elavat muusikat nii kodu kui välismaalt.</h3>
  
-        <h4 className="Header-text5 m-2 m-md-4 text-left">Alternatiivklubi, kus propageeritakse elavat muusikat nii kodu kui välismaalt.</h4>
-        <h2 className="Header-text6 m-2 m-md-5 text-left">Vanim rokiklubi Eestis</h2>
+        {/*<h4 className="Header-text5 m-2 m-md-4 text-left">Alternatiivklubi, kus propageeritakse elavat muusikat nii kodu kui välismaalt.</h4>*/}
+        {/* <h2 className="Header-text6 m-1 m-md-5 text-lg-left ml-lg-5">Vanim rokiklubi Eestis</h2>*/}
       </header>    
     );
   }
