a0718f7cfd7ec11514232891955d560523f63009
77ccreed <kiur.jullis@gmail.com>
2019-01-06 08:53:33 +0200

    ..

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
     return (
     
       <header className="App-header">
-        <h1 className="Header-text display-4 m-2 ml-md-5 text-left"><span className="Header-text2" >Valga</span> Rockiklubi</h1>
+        <h1 className="display-4 m-2 ml-md-5 text-left"><span className="Header-text2" >Valga</span> <span className="Header-text" >Rockiklubi</span></h1>
         <h3 className="Header-text3 m-2 ml-md-5 text-left">Alternatiivklubi, kus propageeritakse elavat muusikat nii kodu kui välismaalt.</h3>
  
         {/*<h4 className="Header-text5 m-2 m-md-4 text-left">Alternatiivklubi, kus propageeritakse elavat muusikat nii kodu kui välismaalt.</h4>*/}
