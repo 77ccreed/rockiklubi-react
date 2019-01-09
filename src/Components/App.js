ef708c685edfbaf9f68462e907abbae38ae10fe9
77ccreed <kiur.jullis@gmail.com>
2019-01-09 10:56:15 +0200

    hamburgermenu

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import React, {
   Component
 } from 'react';
 import "./../Css/App.css";
+import HamburgerMenu from './Navbar';
 import Header from './Header';
 import Header2 from './Header2';
 import Header3 from './Header3';
@@ -13,6 +14,7 @@ class App extends Component {
   render() {
     return ( 
     <div className = "App" >
+      <HamburgerMenu />
       <Header / >
       <main >
       <Header2 / >
