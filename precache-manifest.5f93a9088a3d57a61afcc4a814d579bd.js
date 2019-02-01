f5dd0b30ccae38d4b3b45ca79e1358bda639c6bd
77ccreed <kiur.jullis@gmail.com>
2019-02-01 11:13:35 +0200

    Updates

--- precache-manifest.5f93a9088a3d57a61afcc4a814d579bd.js
+++ precache-manifest.5f93a9088a3d57a61afcc4a814d579bd.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/rockiklubi-react/static/media/15_04_2016.ecfe2637.jpg"
   },
   {
-    "revision": "f63de2e4a106498f75eb",
-    "url": "/rockiklubi-react/static/css/main.39d0cf1f.chunk.css"
+    "revision": "2f7555f395aa1dbedd4f",
+    "url": "/rockiklubi-react/static/css/main.b8a67b39.chunk.css"
   },
   {
     "revision": "388e7a250cdd9aab2a5c1510d23a18fd",
@@ -228,8 +228,8 @@ self.__precacheManifest = [
     "url": "/rockiklubi-react/static/media/14_05_2016.29505eb0.jpg"
   },
   {
-    "revision": "f63de2e4a106498f75eb",
-    "url": "/rockiklubi-react/static/js/main.f63de2e4.chunk.js"
+    "revision": "2f7555f395aa1dbedd4f",
+    "url": "/rockiklubi-react/static/js/main.2f7555f3.chunk.js"
   },
   {
     "revision": "e3a0da28562574b9089276bffb4bf2b5",
@@ -456,7 +456,7 @@ self.__precacheManifest = [
     "url": "/rockiklubi-react/static/css/1.addb2f54.chunk.css"
   },
   {
-    "revision": "37c66fcf464382e0f5f9bbbd3d8327b6",
+    "revision": "11505df0b010c8aa8555ec3bb2d8d27a",
     "url": "/rockiklubi-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rockiklubi-react/precache-manifest.b3658c4c25ccad05c35c99baf3b80909.js"
+  "/rockiklubi-react/precache-manifest.5f93a9088a3d57a61afcc4a814d579bd.js"
 );
 
 workbox.clientsClaim();
